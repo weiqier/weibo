14a6cbd82955962805e612acfacb32dd2ad0cd7f
hakeer <50091329@qq.com>
2019-10-03 17:18:37 +0000

    middleware auth 排除signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,7 @@ class UsersController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', ['except' => ['signup']]);
     }
     /**
      * Display a listing of the resource.
